41b56168b9670db5e84d8475cabd14974d59132e
gangi <gangin0221@gmail.com>
2019-11-25 23:59:45 +0900

    오타 수정 어느정도

---
+++ app/src/main/java/hs/daegun/dg_pr/intro_activity.java
@@ -0,0 +1,14 @@
+package hs.daegun.dg_pr;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class intro_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_activity);
+    }
+}
